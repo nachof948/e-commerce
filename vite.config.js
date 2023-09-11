73a632e0413153f646fa7cfb9b74eef3d4733b51
nachof948 <nachof948@gmail.com>
2023-09-11 15:49:07 -0300

    Avances con el proyecto final

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://nachof948.github.io/e-commerce/'
+  base: '/e-commerce'
 })
